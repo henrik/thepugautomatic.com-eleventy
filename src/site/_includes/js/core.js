95811eba626a3d8e104b36f8fdbce25cefb35dc7
Henrik Nyh <henrik@nyh.se>
2020-03-07 09:57:16 +0000

    Cleanup

--- src/site/_includes/js/core.js
+++ src/site/_includes/js/core.js
@@ -1,10 +1,1 @@
-// simple button click event handler
-function btnHandler(selector, callback) {
-  var btn = document.querySelector(selector);
-  if(!btn) { return; }
-  btn.addEventListener('click', function(event) {
-    event.preventDefault();
-    callback();
-  }, false);
-}
-
+console.log("👋 Hi!");
