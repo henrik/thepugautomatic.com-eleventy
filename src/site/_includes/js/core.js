ebf167d6947e724b921400d8ba7cba54f830bba5
Henrik Nyh <henrik@nyh.se>
2021-02-27 21:48:54 +0000

    Remove third-party ads

--- src/site/_includes/js/core.js
+++ src/site/_includes/js/core.js
@@ -1,1 +1,5 @@
-//console.log("👋 Hi!");
+// Remove certain third-party service ads.
+setInterval(function() {
+  var adFrame = document.querySelector("iframe[src*=ads-iframe]");
+  adFrame && adFrame.remove()
+}, 5000);
